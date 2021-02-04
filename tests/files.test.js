39d563700cfac82c41c9c8e03567d6d4dd9a73cf
Michael Schmidt <mitchi5000.ms@googlemail.com>
2021-02-04 18:29:09 +0100

    test: all files in one directory (#55)
    * test: all files in one directory
    
    * test: one shot file per file
    
    * test: fixed file name

--- tests/files.test.js
+++ tests/files.test.js
@@ -1,12 +1,14 @@
 const prettier = require("prettier");
 const { readFileSync } = require("fs");
 const { resolve } = require("path");
+require("jest-specific-snapshot");
 
 function subjectFiles(relativePath, options = {}) {
   const filepath = resolve(__dirname, relativePath);
 
   try {
     const code = readFileSync(filepath).toString();
+
     return prettier.format(code, {
       plugins: ["."],
       jsdocSpaces: 1,
@@ -19,22 +21,42 @@ function subjectFiles(relativePath, options = {}) {
   }
 }
 
-test("js file", () => {
-  const result = subjectFiles("./files/typeScript.js");
-  expect(result).toMatchSnapshot();
-});
+/**
+ * @type {TestFile[]}
+ *
+ * @typedef TestFile
+ * @property {string} name
+ * @property {import("prettier").Options} [options]
+ */
+const files = [
+  { name: "typeScript.js" },
+  { name: "typeScript.js" },
+  { name: "typeScript.ts" },
+  { name: "types.ts" },
+  { name: "order.jsx" },
+  { name: "create-ignorer.js" },
+  {
+    name: "prism-core.js",
+    options: {
+      arrowParens: "avoid",
+      printWidth: 120,
+      quoteProps: "preserve",
+      semi: true,
+      singleQuote: true,
+      tabWidth: 4,
+      trailingComma: "none",
+      useTabs: true,
+      jsdocKeepUnParseAbleExampleIndent: true,
+    },
+  },
+];
 
-test("ts file", () => {
-  const result = subjectFiles("./files/typeScript.ts");
-  expect(result).toMatchSnapshot();
-});
-
-test("types file", () => {
-  const result = subjectFiles("./files/types.ts");
-  expect(result).toMatchSnapshot();
-});
-
-test("order file", () => {
-  const result = subjectFiles("./files/order.jsx");
-  expect(result).toMatchSnapshot();
-});
+for (let i = 0; i < files.length; i++) {
+  const { name, options } = files[i];
+  test(`File: ${name} Options: ${JSON.stringify(options || {})}`, () => {
+    const result = subjectFiles("./files/" + name, options);
+    expect(result).toMatchSpecificSnapshot(
+      `./__snapshots__/files/${name}.shot`,
+    );
+  });
+}
