1d7136e26f041461b9a5a33784d827a157a1f183
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-02-04 17:00:43 +0100

    test: all files in one directory

--- tests/files.test.js
+++ tests/files.test.js
@@ -7,6 +7,7 @@ function subjectFiles(relativePath, options = {}) {
 
   try {
     const code = readFileSync(filepath).toString();
+
     return prettier.format(code, {
       plugins: ["."],
       jsdocSpaces: 1,
@@ -19,22 +20,39 @@ function subjectFiles(relativePath, options = {}) {
   }
 }
 
-test("js file", () => {
-  const result = subjectFiles("./files/typeScript.js");
-  expect(result).toMatchSnapshot();
-});
+/**
+ * @type {TestFile[]}
+ *
+ * @typedef TestFile
+ * @property {string} path
+ * @property {import("prettier").Options} [options]
+ */
+const files = [
+  { path: "./files/typeScript.js" },
+  { path: "./files/typeScript.js" },
+  { path: "./files/typeScript.ts" },
+  { path: "./files/types.ts" },
+  { path: "./files/order.jsx" },
+  { path: "./files/create-ignorer.js" },
+  {
+    path: "./files/prism-core.js",
+    options: {
+      arrowParens: "avoid",
+      printWidth: 120,
+      quoteProps: "preserve",
+      semi: true,
+      singleQuote: true,
+      tabWidth: 4,
+      trailingComma: "none",
+      useTabs: true,
+      jsdocKeepUnParseAbleExampleIndent: true,
+    },
+  },
+];
 
-test("ts file", () => {
-  const result = subjectFiles("./files/typeScript.ts");
-  expect(result).toMatchSnapshot();
-});
-
-test("types file", () => {
-  const result = subjectFiles("./files/types.ts");
-  expect(result).toMatchSnapshot();
-});
-
-test("order file", () => {
-  const result = subjectFiles("./files/order.jsx");
-  expect(result).toMatchSnapshot();
-});
+for (const { path, options } of files) {
+  test(`File: ${path} Options: ${JSON.stringify(options || {})}`, () => {
+    const result = subjectFiles(path, options);
+    expect(result).toMatchSnapshot();
+  });
+}
