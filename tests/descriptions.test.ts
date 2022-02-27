092f409097465746a8c2eb549c958c1837c94a8f
jason lim <50891910+Sxxov@users.noreply.github.com>
2022-02-28 05:41:39 +0800

    fix: line breaks inside jsdoc link synonyms (#149)

--- tests/descriptions.test.ts
+++ tests/descriptions.test.ts
@@ -805,6 +805,61 @@ test("Jsdoc link in description", () => {
   expect(result1).toMatchSnapshot();
 });
 
+test("Jsdoc link synonyms in description", () => {
+  const result1 = subject(`
+/**
+ * Calculate the 
+ * {@linkcode https://en.wikipedia.org/wiki/Complement_(set_theory)#Relative_complement difference}
+ * between two sets.
+ * @param second
+ * @param first
+ */
+ export function difference<T>(first: Set<T>, second: Set<T>): Set<T>
+
+
+ /**
+ * Calculate the 
+ * {@linkcode https://en.wikipedia.org/wiki/Complement_(set_theory)#Relative_complement difference}
+ * {@linkcode https://en.wikipedia.org/wiki/Complement_(set_theory)#Relative_complement difference}
+ * between two sets.
+ */
+
+  /**
+ * Calculate the 
+ * {@linkcode https://en.wikipedia.org/wiki/Complement_(set_theory)#Relative_complement difference}
+ * between
+ * {@linkcode https://en.wikipedia.org/wiki/Complement}
+ * between two sets.
+ */
+
+  /**
+ * Calculate the 
+ * {@linkplain https://en.wikipedia.org/wiki/Complement_(set_theory)#Relative_complement difference}
+ * between two sets.
+ * @param second
+ * @param first
+ */
+ export function difference<T>(first: Set<T>, second: Set<T>): Set<T>
+
+
+ /**
+ * Calculate the 
+ * {@linkplain https://en.wikipedia.org/wiki/Complement_(set_theory)#Relative_complement difference}
+ * {@linkplain https://en.wikipedia.org/wiki/Complement_(set_theory)#Relative_complement difference}
+ * between two sets.
+ */
+
+  /**
+ * Calculate the 
+ * {@linkplain https://en.wikipedia.org/wiki/Complement_(set_theory)#Relative_complement difference}
+ * between
+ * {@linkplain https://en.wikipedia.org/wiki/Complement}
+ * between two sets.
+ */`);
+
+  expect(result1).toMatchSnapshot();
+});
+
 test("Markdown link", () => {
   const result1 = subject(`
 /**
