2f5de4fe5b30997941122ff9a872782065dd2229
Hossein Mohammadi <hosseinm.developer@gmail.com>
2023-10-05 23:39:20 +0330

    test: add tagGroup test
    issue: https://github.com/hosseinmd/prettier-plugin-jsdoc/issues/191

--- tests/tagGroup.test.ts
+++ tests/tagGroup.test.ts
@@ -90,3 +90,31 @@ test("space after unknownTag", async () => {
 
   expect(await _subject(await _subject(result))).toMatchSnapshot();
 });
+
+test("Inconsistant formatting", async () => {
+  const result = await subject(
+    `
+    /**
+     * Aliquip ex proident tempor eiusmod aliquip amet. Labore commodo nulla tempor
+     * consequat exercitation incididunt non. Duis laboris reprehenderit proident
+     * proident.
+     *
+     * @example
+     *   const foo = 0;
+     *
+     *
+     * @param id A test id.
+     *
+     * @throws Minim sit ad commodo ut dolore magna magna minim consequat. Ex
+     *   consequat esse incididunt qui voluptate id voluptate quis ex et. Ullamco
+     *   cillum nisi amet fugiat.
+     * @see {@link http://acme.com}
+     */
+`,
+    {
+      jsdocSeparateTagGroups: true,
+    },
+  );
+
+  expect(result).toMatchSnapshot();
+});
